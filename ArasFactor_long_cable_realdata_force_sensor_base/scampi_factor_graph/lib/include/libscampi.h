dac831f0e81135d580f550ca5cf10f6b50ce3359
MohammadrezaDindarloo <M.dindarloo@email.kntu.ac.ir>
2024-02-29 19:37:50 +0330

    real data test using one force sensor completed

---
+++ ArasFactor_long_cable_realdata_force_sensor_base/scampi_factor_graph/lib/include/libscampi.h
@@ -0,0 +1,15 @@
+#include "scampi_fg_data_types.h"
+
+#pragma once
+
+#include "scampi_fg_IK.h"
+#include "scampi_fg_FK.h"
+#include "../include/scampi_fg_utils.h"
+
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+#include <vector>
+#include <cmath>
+#include <manif/manif.h>
+#include <ceres/ceres.h>
